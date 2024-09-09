4f1b5dc5a0536050876fa4e294715fc456ac8124
kremendmitriy <kremen.dima25@gmail.com>
2024-09-09 09:12:18 +0200

    creating functionality for favorite cards, fixing a bug with a button component

---
+++ src/components/Card.jsx
@@ -0,0 +1,104 @@
+import { Link } from 'react-router-dom';
+import styled from '@emotion/styled';
+import { Button } from '../components/Button';
+import { Badge } from '../components/Badge';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+
+const CardContainerNew = ({
+   className,
+   id,
+   userName,
+   title,
+   imageUrl,
+   role,
+   isFavorite,
+   onToggleFavorite,
+}) => {
+   return (
+      <div className={className}>
+         <div className="cards">
+            <div className="card-info">
+               <img src={imageUrl} alt={userName} />
+               <div className="name-and-button">
+                  <h2>{userName}</h2>
+                  <Link to={`/cards/${id}`}>
+                     <Button borderRadius="5px" backgroundColor="#eb465f">
+                        Перейти к карточке
+                     </Button>
+                  </Link>
+               </div>
+               <div className="bages">
+                  <div onClick={() => onToggleFavorite(id)}>
+                     <FavoriteBorderIcon
+                        style={{
+                           color: isFavorite ? 'red' : 'inherit',
+                           cursor: 'pointer',
+                        }}
+                     />
+                  </div>
+                  <Badge role={role} />
+               </div>
+            </div>
+            <div className="post-card-info">{title}</div>
+         </div>
+      </div>
+   );
+};
+
+export const Card = styled(CardContainerNew)`
+   & a {
+      text-decoration: none;
+      color: #ddd;
+      height: 80%;
+   }
+
+   & button {
+      margin-top: 20px;
+      color: white;
+      padding: 10px 20px;
+      font-size: 16px;
+      text-align: center;
+      cursor: pointer;
+      transition: transform 0.2s ease-in-out;
+
+      &:hover {
+         transform: translateY(-3px);
+      }
+
+      &:focus {
+         outline: none;
+      }
+   }
+
+   & .cards {
+      width: 400px;
+      padding: 20px;
+      min-height: 190px;
+      margin: 20px 15px;
+      border-radius: 8px;
+      box-shadow: 0 3px 5px rgba(0, 0, 0, 0.1);
+   }
+
+   & .card-info {
+      display: flex;
+      justify-content: space-between;
+      width: auto;
+      padding: 10px;
+   }
+
+   & img {
+      width: 100px;
+      height: 100px;
+   }
+
+   & h2 {
+      margin: 10px 0;
+      font-size: 20px;
+   }
+
+   & .post-card-info {
+      padding: 10px 0 0;
+      font-size: 18px;
+      text-align: center;
+   }
+`;
--- src/pages/ParticipantsPage.jsx
+++ src/pages/ParticipantsPage.jsx
@@ -1,127 +1,56 @@
 /** @jsxImportSource @emotion/react */
+import { useState } from 'react';
+import { Card } from '../components/Card';
 
-import userData from "../../db.json";
-import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
-import { Link } from "react-router-dom";
-import { Button } from "../components/Button";
-import styled from "@emotion/styled";
-import { Badge } from "./../components/Badge";
+import userData from '../../db.json';
+import styled from '@emotion/styled';
 
 const ParticipantsPageContainer = ({ className }) => {
-  const users = userData.users;
+   const [favorites, setFavorites] = useState(() => {
+      const savedFavorites = localStorage.getItem('favorites');
+      return savedFavorites ? JSON.parse(savedFavorites) : [];
+   });
+   const users = userData.users;
 
-  return (
-    <div className={className}>
-      <div className="cards-container">
-        {users.map(({ id, title, userName, imageUrl, role, age }) => (
-          <div className="cards" key={id}>
-            <div className="card-info">
-               <div className="image">
-                 <img src={imageUrl} alt={userName} /> 
-               </div>
-              
-              <div className="name-and-badge">
-                <h2>
-                  {userName}, {age}
-                </h2>
-                <Badge role={role} />
-                </div>
-                
-            </div>
-            <div className="post-card-info">{title}</div>
-            <div className="button-and-favorite">
-            <FavoriteBorderIcon className="favorite-icon" />
-                  <Link to={`/cards/${id}`}>
-                    <Button borderRadius="5px" backgroundColor="#eb465f">
-                      Перейти к карточке
-                    </Button>
-                  </Link>
-                  
-                </div>
-          </div>
-        ))}
+   const toggleFavorite = (id) => {
+      let updatedFavorites;
+      if (favorites.includes(id)) {
+         updatedFavorites = favorites.filter((favId) => favId !== id);
+      } else {
+         updatedFavorites = [...favorites, id];
+      }
+
+      setFavorites(updatedFavorites);
+      localStorage.setItem('favorites', JSON.stringify(updatedFavorites));
+   };
+
+   return (
+      <div className={className}>
+         <div className="cards-container">
+            {users.map((user) => (
+               <Card
+                  key={user.id}
+                  id={user.id}
+                  userName={user.userName}
+                  title={user.title}
+                  imageUrl={user.imageUrl}
+                  role={user.role}
+                  isFavorite={favorites.includes(user.id)}
+                  onToggleFavorite={toggleFavorite}
+               />
+            ))}
+         </div>
       </div>
-    </div>
-  );
+   );
 };
 export const ParticipantsPage = styled(ParticipantsPageContainer)`
-  & a {
-    text-decoration: none;
-    color: #ddd;
-    height: 80%;
-  }
-  .cards-container {
-    display: flex;
-    flex-wrap: wrap;
-    gap: 20px;
-  }
+   display: flex;
+   justify-content: space-between;
+   flex-direction: column;
 
-  .cards {
-    display: flex;
-    flex-direction: column;
-    border: 1px solid #ddd;
-    padding: 20px;
-    width: 400px;
-    padding: 20px;
-    margin: 20px 15px;
-    border-radius: 8px;
-    box-shadow: 0 3px 5px rgba(0, 0, 0, 0.1);
-  }
-
-  .card-info {
-    display: flex;
-    align-items: center;
-    text-align: center;
-  }
-
-  .card-info img {
-    width: 100px;
-    height: 100px;
-  }
-  & h2 {
-    margin: 0 10px 0  0;
-    font-size: 18px;
-  }
-  .name-and-badge {
-    display: flex;
-    margin: 0 0 0 10px;
-  }
-
-  .button-and-favorite {
-    display: flex;
-    justify-content: flex-end;
-    align-items: center;
-    gap: 10px;
-    margin-left: 20px;
-
-    & button {
-      color: white;
-      padding: 10px 20px;
-      font-size: 16px;
-      text-align: center;
-      cursor: pointer;
-      transition: transform 0.2s ease-in-out;
-
-      &:hover {
-        transform: translateY(-3px);
-      }
-
-      &:focus {
-        outline: none;
-      }
-    }
-  }
-
-  .favorite-icon {
-    width: 50px;
-    margin-top: 15px;
-    cursor: pointer;
-  }
-
-  .post-card-info {
-    margin: 5px  0 0 0;
-    text-align: left;
-    font-size: 18px;
-    height: 50px;
-  }
+   & .cards-container {
+      display: flex;
+      flex-wrap: wrap;
+      padding: 20px 20px 80px;
+   }
 `;
